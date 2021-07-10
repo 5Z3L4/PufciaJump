feb09a9ac832e49a49d622ccc3fa78b18846fe7e
5Z3L4 <58202728+5Z3L4@users.noreply.github.com>
2021-07-10 02:48:49 +0200

    TimeJumpOffset

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]private float moveSpeed;
-    //public Rigidbody Rb;
     public CharacterController controller;
     public float jumpForce;
     public float graviyScale;
+    public float jumpPressedRemember;
+    public float jumpPressedRememberTime = 0.5f;
 
     private Vector3 moveDirection;
     // Start is called before the first frame update
@@ -26,13 +27,19 @@ public class PlayerController : MonoBehaviour
         moveDirection = (transform.forward * Input.GetAxisRaw("Vertical")) + (transform.right * Input.GetAxisRaw("Horizontal"));
         moveDirection = moveDirection.normalized * moveSpeed;
         moveDirection.y = yStore;
+        jumpPressedRemember -= Time.deltaTime;
      
+        if (Input.GetButtonDown("Jump"))
+        {
+            jumpPressedRemember = jumpPressedRememberTime;
+        }
         if (controller.isGrounded)
         {
             moveDirection.y = -1;
 
-            if (Input.GetButtonDown("Jump"))
+            if (jumpPressedRemember > 0)
             {
+                jumpPressedRemember = 0;
                 moveDirection.y = jumpForce;
             }
 
